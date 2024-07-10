c3428e570e466cc239a8444a7e90ae1e3102bbf6
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-07-10 12:36:18 +0200

    u

--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -16,7 +16,8 @@
         <?php endforeach; ?>
 
         <div class="mt-4">
-            <a href="/notes/create" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</a>
+            <a href="/notes/create"
+               class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</a>
         </div>
     </div>
 
