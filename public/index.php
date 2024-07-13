b1611c7aeaacdad6a07b776e94baec2df563ad19
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-07-13 10:48:47 +0200

    update

--- public/index.php
+++ public/index.php
@@ -2,8 +2,11 @@
 
 use Core\Router;
 
-const BASE_PATH = __DIR__ . '/../';
-require BASE_PATH . "/Core/functions.php";
+session_start();
+$_SESSION['name'] = 'Hello , Ajdin';
+
+const BASE_PATH = __DIR__.'/../';
+require BASE_PATH."/Core/functions.php";
 
 
 spl_autoload_register(function ($class) {
