c55bb91dc2ff73d7d609570dee18788b9470895c
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-07-30 12:04:56 +0200

    uuuu

--- views/sessions/create.view.php
+++ views/sessions/create.view.php
@@ -18,7 +18,10 @@
                             address</label>
                         <div class="mt-2">
                             <input id="email" name="email" type="email" autocomplete="email"
-                                   class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                   value="<?= old('email') ?>"
+                                   class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1
+                            ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                            focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                         <?php if (isset($errors['email'])) : ?>
                             <p class="mt-1 text-xs font-semibold leading-6 text-red-600"> <?= $errors['email'] ?> </p>
