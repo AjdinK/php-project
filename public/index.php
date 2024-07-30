433a7ce69409e1e27f688a728125f364572ae061
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-07-30 19:51:24 +0200

    update

--- public/index.php
+++ public/index.php
@@ -4,18 +4,15 @@ use Core\Router;
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
-$_SESSION['name'] = 'Hello , Ajdin';
-
 const BASE_PATH = __DIR__.'/../';
+require BASE_PATH.'/vendor/autoload.php';
+
+session_start();
+
 require BASE_PATH."/Core/functions.php";
 require base_path("Core/Middleware/Middleware.php");
 require base_path("Core/Authenticator.php");
 
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
 
 require base_path('bootstrap.php');
 
